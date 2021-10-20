0c6f1e8b044030f3c64749d57107ccb10fb459a1
abc <abc@xyz.com>
2021-10-20 14:08:10 +0530

    Initial commit.

---
+++ HibernateCacheExampleFull/src/main/java/com/javalive/hibernate/EntityCollectionCacheExample.java
@@ -0,0 +1,61 @@
+package com.javalive.hibernate;
+
+import java.util.List;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.javalive.entity.Department;
+import com.javalive.entity.Employee;
+
+/**
+ * @author javalive.com 
+ *         Earlier we have annotated the Employee and Department
+ *         entities and List<Employee> property with the @Cache annotation. To
+ *         know whether the @Cache annotation is working or not, create an
+ *         EntityCollectionCacheExample class and write the following code in
+ *         it.
+ */
+public class EntityCollectionCacheExample {
+	private static final Logger logger = Logger.getLogger(EntityCollectionCacheExample.class);
+	public static void main(String[] args) {
+		BasicConfigurator.configure(); //enough for configuring log4j
+		Logger.getRootLogger().setLevel(Level.DEBUG); //changing log level
+		Session session = null;
+		Transaction transaction = null;
+		try {
+			// Get Department from DATABASE
+			session = HibernateUtil.getSessionFactory().openSession();
+			transaction = session.getTransaction();
+			transaction.begin();
+			Department department = session.get(Department.class, 11L);
+			System.out.println("Department :" + department.getName());
+			List<Employee> employees = department.getEmployees();
+			for (Employee employee : employees) {
+				System.out.println("\tEmployee Name : " + employee.getName());
+			}
+			transaction.commit();
+			session.close();
+
+			// Get Department from Cache
+			session = HibernateUtil.getSessionFactory().openSession();
+			transaction = session.getTransaction();
+			transaction.begin();
+			Department department2 = session.get(Department.class, 11L);
+			System.out.println("Department :" + department2.getName());
+			List<Employee> employees2 = department2.getEmployees();
+			for (Employee employee : employees2) {
+				System.out.println("\tEmployee Name : " + employee.getName());
+			}
+			transaction.commit();
+			session.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		HibernateUtil.shutdown();
+	}
+}
---
+++ HibernateCacheExampleFull/src/main/java/com/javalive/hibernate/QueryCacheExample.java
@@ -0,0 +1,73 @@
+package com.javalive.hibernate;
+
+import java.util.List;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.javalive.entity.Employee;
+
+/**
+ * @author javalive.com 
+ * 		   Hibernate Query Cache to avoid amount of traffic between
+ *         your application and database. The query cache is responsible for
+ *         caching the results of queries. Let us have a look how Hibernate uses
+ *         the query cache to retrieve objects. Note that the query cache does
+ *         not cache the state of the actual entities in the result set; it
+ *         caches only identifier values and results of value type. So the query
+ *         cache should always be used in conjunction with the second-level
+ *         cache. 
+ *         The combination of the query and the values provided as
+ *         parameters to that query is used as a key, and the value is the list
+ *         of identifiers for that query. Note that this becomes more complex
+ *         from an internal perspective as you begin to consider that a query
+ *         can have an effect of altering associations to objects returned that
+ *         query; not to mention the fact that a query may not return whole
+ *         objects, but may in fact only return scalar values (when you have
+ *         supplied a select clause for instance). That being said, this is a
+ *         sound and reliable way to think of the query cache conceptually.
+ */
+public class QueryCacheExample {
+	final static Logger logger = Logger.getLogger(QueryCacheExample.class);
+	public static void main(String[] args) {
+		BasicConfigurator.configure(); //enough for configuring log4j
+		Logger.getRootLogger().setLevel(Level.DEBUG); //changing log level
+		Session session = null;
+		Transaction transaction = null;
+		try {
+
+			// Get employee list from DATABASE
+			session = HibernateUtil.getSessionFactory().openSession();
+			transaction = session.getTransaction();
+			transaction.begin();
+			@SuppressWarnings("unchecked")
+			List<Employee> employees = session.createQuery("from Employee").setCacheable(true)//*****IMP
+					.setCacheRegion("employee.cache").list();
+			for (Employee employee : employees) {
+				System.out.println("\tEmployee Name : " + employee.getName());
+			}
+			transaction.commit();
+			session.close();
+
+			/// Get employee list from Cached result
+			session = HibernateUtil.getSessionFactory().openSession();
+			transaction = session.getTransaction();
+			transaction.begin();
+			@SuppressWarnings("unchecked")
+			List<Employee> employees2 = session.createQuery("from Employee").setCacheable(true)//*****IMP
+					.setCacheRegion("employee.cache").list();
+			for (Employee employee : employees2) {
+				System.out.println("\tEmployee Name : " + employee.getName());
+			}
+			transaction.commit();
+			session.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		HibernateUtil.shutdown();
+	}
+}
